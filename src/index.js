391429290390a4038374a1cc6097accbb5bef0c5
tokisuno <lucaswmei@gmail.com>
2025-05-13 14:10:37 -0400

    starting this before todo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import "./styles.css";
-import shakeImage from "./shake.jpeg";
 
-import { greeting } from "./greeting.js";
-
-const image = document.createElement("img");
-image.src = shakeImage;
-
-document.body.appendChild(image);
-
-console.log(greeting);
+console.log("this is working");
